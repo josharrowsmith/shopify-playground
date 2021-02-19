d0bebd5cb7959dad0502145f1463a64c63ef4704
josh.arrowsmith@hotmail.com <josh.arrowsmith@hotmail.com>
2021-02-20 01:40:08 +1000

    framer motion test

--- scripts/Components/CartToolTip.js
+++ scripts/Components/CartToolTip.js
@@ -1,8 +1,15 @@
 //@ts-check
 import React, { useEffect, useState } from "react"
+import { motion } from "framer-motion"
 
 export const CartToolTip = () => {
   return (
-    <h1>hey dude</h1>
+    <motion.div
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 270, 270, 0],
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}
+    />
   )
 }
