a86f7e8e1e0f54a1fa58e562eae725fec05c2f70
josh.arrowsmith@hotmail.com <josh.arrowsmith@hotmail.com>
2021-08-08 00:51:26 +1000

    checkout working

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,9 +1,12 @@
 /* global module */
 module.exports = {
-  plugins: ['react', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   parserOptions: {
@@ -20,6 +23,7 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   ignorePatterns: ['build/*'],
 };
